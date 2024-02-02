f7d2d41b89259ce7c95b76128e761719aa72e1db
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-02-02 19:38:30 +0000

    final commit

--- app.rb
+++ app.rb
@@ -1,9 +1,66 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# configuration for better_errors
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:home)  
+end
+
+get("/rock") do
+  
+  @user = "rock"
+  @computer = ["rock", "paper", "scissors"].sample
+
+  if @user == @computer
+    "It's a tie!"
+  elsif (@user == "rock" && @computer == "scissors") 
+        @result = "You win!"
+  else
+  "Computer wins!"
+  end
+
+  erb(:play_rock)
+end
+
+  get("/paper") do
+    
+  @user = "paper"
+  @computer = ["rock", "paper", "scissors"].sample
+
+  if @user == @computer
+    "It's a tie!"
+  elsif (@user == "paper" && @computer == "rock") 
+   @result = "You win!"
+  else
+  "Computer wins!"
+  end
+  erb(:play_paper)
+
+ end
+get("/scissors") do
+
+  @user = "scissors"
+  @computer = ["rock", "paper", "scissors"].sample
+  
+  if @user == @computer
+    "It's a tie!"
+  elsif(@user == "scissors" && @computer == "paper")
+   @result = "You win!"
+  else
+  "Computer wins!"
+  end
+
+  erb(:play_scissors)
 end
